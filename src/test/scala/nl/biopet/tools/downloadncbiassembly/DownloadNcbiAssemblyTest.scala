34331ecbf72ec528006c3b475d6be3704d287f5c
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/test/scala/nl/biopet/tools/downloadncbiassembly/DownloadNcbiAssemblyTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.downloadncbiassembly
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object DownloadNcbiAssemblyTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
