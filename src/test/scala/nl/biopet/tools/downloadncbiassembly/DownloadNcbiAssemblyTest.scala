7cd67c4f61dc2c28029a73a7d30e5a91599a61b6
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/downloadncbiassembly/DownloadNcbiAssemblyTest.scala
+++ src/test/scala/nl/biopet/tools/downloadncbiassembly/DownloadNcbiAssemblyTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.downloadncbiassembly
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object DownloadNcbiAssemblyTest extends BiopetTest {
+class DownloadNcbiAssemblyTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
