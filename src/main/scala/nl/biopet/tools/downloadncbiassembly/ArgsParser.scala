86fc9cf5f58c0968c6824c7b6b834a9b85632930
DavyCats <davycats.dc@gmail.com>
2017-10-13 15:38:04 +0200

    tool code

--- src/main/scala/nl/biopet/tools/downloadncbiassembly/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/downloadncbiassembly/ArgsParser.scala
@@ -5,10 +5,31 @@ import java.io.File
 import nl.biopet.utils.tool.AbstractOptParser
 
 class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
-  opt[File]("inputFile")
-    .abbr("i")
-    .unbounded()
-    .required()
-    .maxOccurs(1)
-    .action((x, c) => c.copy(inputFile = x))
+  opt[File]('a', "assembly_report") required () unbounded () valueName "<file>" action {
+    (x, c) =>
+      c.copy(assemblyReport = x)
+  } text "refseq ID from NCBI"
+  opt[File]('o', "output") required () unbounded () valueName "<file>" action { (x, c) =>
+    c.copy(outputFile = x)
+  } text "output Fasta file"
+  opt[File]("report") unbounded () valueName "<file>" action { (x, c) =>
+    c.copy(reportFile = Some(x))
+  } text "where to write report from ncbi"
+  opt[String]("nameHeader") unbounded () valueName "<string>" action { (x, c) =>
+    c.copy(contigNameHeader = Some(x))
+  } text
+    """
+      | What column to use from the NCBI report for the name of the contigs.
+      | All columns in the report can be used but this are the most common field to choose from:
+      | - 'Sequence-Name': Name of the contig within the assembly
+      | - 'UCSC-style-name': Name of the contig used by UCSC ( like hg19 )
+      | - 'RefSeq-Accn': Unique name of the contig at RefSeq (default for NCBI)""".stripMargin
+  opt[(String, String)]("mustHaveOne") unbounded () valueName "<column_name=regex>" action {
+    (x, c) =>
+      c.copy(mustHaveOne = (x._1, x._2) :: c.mustHaveOne)
+  } text "This can be used to filter based on the NCBI report, multiple conditions can be given, at least 1 should be true"
+  opt[(String, String)]("mustNotHave") unbounded () valueName "<column_name=regex>" action {
+    (x, c) =>
+      c.copy(mustNotHave = (x._1, x._2) :: c.mustNotHave)
+  } text "This can be used to filter based on the NCBI report, multiple conditions can be given, all should be false"
 }
--- src/main/scala/nl/biopet/tools/downloadncbiassembly/DownloadNcbiAssembly.scala
+++ src/main/scala/nl/biopet/tools/downloadncbiassembly/DownloadNcbiAssembly.scala
@@ -1,6 +1,10 @@
 package nl.biopet.tools.downloadncbiassembly
 
+import java.io.PrintWriter
+
 import nl.biopet.utils.tool.ToolCommand
+
+import scala.io.Source
 
 object DownloadNcbiAssembly extends ToolCommand {
   def main(args: Array[String]): Unit = {
@@ -10,8 +14,68 @@ object DownloadNcbiAssembly extends ToolCommand {
 
     logger.info("Start")
 
-    //TODO: Execute code
+    downloadNcbiassembly(cmdArgs)
 
     logger.info("Done")
+  }
+
+  def downloadNcbiassembly(cmdArgs: Args): Unit = {
+    logger.info(s"Reading ${cmdArgs.assemblyReport}")
+    val reader = Source.fromFile(cmdArgs.assemblyReport)
+    val assamblyReport = reader.getLines().toList
+    reader.close()
+    cmdArgs.reportFile.foreach { file =>
+      val writer = new PrintWriter(file)
+      assamblyReport.foreach(writer.println)
+      writer.close()
+    }
+
+    val headers = assamblyReport
+      .filter(_.startsWith("#"))
+      .last
+      .stripPrefix("# ")
+      .split("\t")
+      .zipWithIndex
+      .toMap
+    val nameId = cmdArgs.contigNameHeader.map(x => headers(x))
+    val lengthId = headers.get("Sequence-Length")
+
+    val baseUrlEutils = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
+
+    val fastaWriter = new PrintWriter(cmdArgs.outputFile)
+
+    val allContigs = assamblyReport
+      .filter(!_.startsWith("#"))
+      .map(_.split("\t"))
+    val totalLength = lengthId.map(id => allContigs.map(_.apply(id).toLong).sum)
+
+    logger.info(s"${allContigs.size} contigs found")
+    totalLength.foreach(l => logger.info(s"Total length: $l"))
+
+    val filterContigs = allContigs
+      .filter(values => cmdArgs.mustNotHave.forall(x => values(headers(x._1)) != x._2))
+      .filter(values =>
+        cmdArgs.mustHaveOne
+          .exists(x => values(headers(x._1)) == x._2) || cmdArgs.mustHaveOne.isEmpty)
+    val filterLength = lengthId.map(id => filterContigs.map(_.apply(id).toLong).sum)
+
+    logger.info(s"${filterContigs.size} contigs left after filtering")
+    filterLength.foreach(l => logger.info(s"Filtered length: $l"))
+
+    filterContigs.foreach { values =>
+      val id = if (values(6) == "na") values(4) else values(6)
+      logger.info(s"Start download $id")
+      val fastaReader =
+        Source.fromURL(s"$baseUrlEutils/efetch.fcgi?db=nuccore&id=$id&retmode=text&rettype=fasta")
+      fastaReader
+        .getLines()
+        .map(x => nameId.map(y => x.replace(">", s">${values(y)} ")).getOrElse(x))
+        .foreach(fastaWriter.println)
+      fastaReader.close()
+    }
+
+    logger.info("Downloading complete")
+
+    fastaWriter.close()
   }
 }
