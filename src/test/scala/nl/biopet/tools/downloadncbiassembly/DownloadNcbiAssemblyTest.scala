032366ea9114b56e0245701d48c511051d3a36df
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/downloadncbiassembly/DownloadNcbiAssemblyTest.scala
+++ src/test/scala/nl/biopet/tools/downloadncbiassembly/DownloadNcbiAssemblyTest.scala
@@ -1,5 +1,25 @@
-package nl.biopet.tools.downloadncbiassembly
+/*
+ * Copyright (c) 2014 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
+package nl.biopet.tools.downloadncbiassembly
 
 import java.io.File
 
@@ -25,11 +45,11 @@ class DownloadNcbiAssemblyTest extends ToolTest[Args] {
     outputReport.deleteOnExit()
     DownloadNcbiAssembly.main(
       Array("-a",
-        new File(resourcePath("/GCF_000844745.1.report")).getAbsolutePath,
-        "-o",
-        output.getAbsolutePath,
-        "--report",
-        outputReport.getAbsolutePath))
+            new File(resourcePath("/GCF_000844745.1.report")).getAbsolutePath,
+            "-o",
+            output.getAbsolutePath,
+            "--report",
+            outputReport.getAbsolutePath))
 
     Source.fromFile(output).getLines().toList shouldBe Source
       .fromFile(new File(resourcePath("/NC_003403.1.fasta")))
